1d4d4618e47457067fd07e17c840e58f0213c684
Stephen Holsenbeck <sholsen@alumni.emory.edu>
2023-11-16 07:32:08 -0500

    Remove unneeded dependencies

--- R/deps.R
+++ R/deps.R
@@ -5,11 +5,11 @@ add_bs4Dash_deps <- function(tag, options) {
   adminLTE3_js <- "adminlte.min.js"
   adminLTE3_css <- "adminlte.min.css"
   bs4Dash_css <- "bs4Dash.css"
-  jquery_ui_js <- "jquery-ui.min.js"
+  # jquery_ui_js <- "jquery-ui.min.js"
   bootstrap_js <- "bootstrap.bundle.min.js"
   os_js <- "jquery.overlayScrollbars.min.js"
   os_css <- "OverlayScrollbars.min.css"
-  fontawesome_css <- "https://use.fontawesome.com/releases/v5.0.13/css/"
+  # fontawesome_css <- "https://use.fontawesome.com/releases/v5.0.13/css/"
   ionicons_css <- "https://unpkg.com/ionicons@4.4.2/dist/css/"
   google_fonts <- "https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700"
   
@@ -17,12 +17,12 @@ add_bs4Dash_deps <- function(tag, options) {
   
   bs4Dash_deps <- list(
     # jquery UI deps for sortable elements
-    htmltools::htmlDependency(
-      name = "jquery-ui", 
-      version = "1.12.1",
-      src = c(file = system.file("jquery-ui-1.12.1", package = "bs4Dash")),
-      script = jquery_ui_js
-    ),
+    # htmltools::htmlDependency(
+    #   name = "jquery-ui", 
+    #   version = "1.12.1",
+    #   src = c(file = system.file("jquery-ui-1.12.1", package = "bs4Dash")),
+    #   script = jquery_ui_js
+    # ),
     # bootstrap deps
     htmltools::htmlDependency(
       name = "bootstrap", 
@@ -36,7 +36,7 @@ add_bs4Dash_deps <- function(tag, options) {
       name = "os", 
       version = "1.13.0",
       src = c(file = "os-1.13.0"),
-      script = os_js,
+      script = list(src = os_js, defer = NA),
       stylesheet = os_css,
       package = "bs4Dash"
     ),
@@ -61,7 +61,7 @@ add_bs4Dash_deps <- function(tag, options) {
     },
     # adminLTE3 deps
     htmltools::htmlDependency(
-      name = "AdminLTE3", 
+      name = "AdminLTE3",
       version = "3.1.0",
       src = c(file = "AdminLTE3-3.1.0"),
       script = adminLTE3_js,
@@ -76,7 +76,7 @@ add_bs4Dash_deps <- function(tag, options) {
       script = if (getOption("shiny.minified", TRUE)) "bs4Dash.min.js" else "bs4Dash.js" ,
       stylesheet = bs4Dash_css,
       package = "bs4Dash"
-    ),
+    )
     # fontawesome Commented since Shiny already loads it with icon...
     #htmltools::htmlDependency(
     #  name = "fontawesome",
@@ -85,27 +85,27 @@ add_bs4Dash_deps <- function(tag, options) {
     #  stylesheet = "all.css"
     #),
     # ionicons
-    htmltools::htmlDependency(
-      name = "ionicons",
-      version = "4.4.2",
-      src = c(href = ionicons_css),
-      stylesheet = "ionicons.min.css"
-    ),
+    # htmltools::htmlDependency(
+    #   name = "ionicons",
+    #   version = "4.4.2",
+    #   src = c(href = ionicons_css),
+    #   stylesheet = "ionicons.min.css"
+    # ),
     # google fonts
-    htmltools::htmlDependency(
-      name = "googlefonts",
-      version = bs4Dash_version,
-      src = c(href = google_fonts),
-      stylesheet = ""
-    ),
+    # htmltools::htmlDependency(
+    #   name = "googlefonts",
+    #   version = bs4Dash_version,
+    #   src = c(href = google_fonts),
+    #   stylesheet = ""
+    # ),
     # glyphicons
-    htmltools::htmlDependency(
-      name = "glyphicons",
-      version = bs4Dash_version,
-      src = c(file = "glyphicons"),
-      stylesheet = "glyphicons.min.css",
-      package = "bs4Dash"
-    )
+    # htmltools::htmlDependency(
+    #   name = "glyphicons",
+    #   version = bs4Dash_version,
+    #   src = c(file = "glyphicons"),
+    #   stylesheet = "glyphicons.min.css",
+    #   package = "bs4Dash"
+    # )
   )
   
   shiny::tagList(tag, bs4Dash_deps)
